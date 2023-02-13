df4090560bcb34e854cf0197810e743b9c2ab5f3
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-02-13 20:38:19 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+# 슬라이싱
+c = '123456789'
+
+print(c[0:4])  # 이상:미만:간격
+print(c[:5])  # 이상:미만:간격 처음부터 5번 전까지
+print(c[6:])  # 이상:미만:간격 5부터 끝까지
+print(c[::2])  # 이상:미만:간격
+
+number = 10
+a = f"I eat {number} apples."
+print(a)
+
+b = "hobby"
+print(b.find('y'))  # y가 몇번째 자리에 있는 지 찾는 거 0,1,2,3,4

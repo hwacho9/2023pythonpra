d9e4452edb4fc13f2f5edfd366781f891d039f04
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-02-14 22:44:34 +0900

    list

---
+++ datastructures.py
@@ -0,0 +1,8 @@
+day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]  # list
+
+day_of_week.append("Sat")
+print(day_of_week)
+day_of_week.append("Sun")
+print(day_of_week)
+day_of_week.remove("Sun")
+print(day_of_week)

a26cfb3acc473a38773229f31d93251c72a2ec9e
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-02-14 18:57:39 +0900

    while

--- controlflow.py
+++ controlflow.py
@@ -1,3 +1,5 @@
+from random import randint, random
+
 a = 10
 b = 15
 
@@ -28,12 +30,17 @@ elif age == 60 or age == 70:
 else:
     print("DRINK SOJU!")
 
-user_choice = int(input("Choose number."))
-pc_choice = 50
+print("Welcome to Python Casino")
+pc_choice = randint(1, 50)
 
-if user_choice == pc_choice:
-    print("You won!")
-elif user_choice > pc_choice:
-    print("Lower!")
-elif user_choice < pc_choice:
-    print("Higer!")
+playing = True
+
+while playing:
+    user_choice = int(input("Choose number."))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower! Computer chose", pc_choice)
+    elif user_choice < pc_choice:
+        print("Higher! Computer chose", pc_choice)

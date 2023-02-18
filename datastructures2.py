2deeb487cf9948bc70cbcabd858fa1cc08037538
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-02-18 16:21:11 +0900

    datastructure2

--- datastructures2.py
+++ datastructures2.py
@@ -1,3 +1,5 @@
+from requests import get
+
 websites = (
     "google.com",
     "airbnb.com",
@@ -5,8 +7,17 @@ websites = (
     "facebook.com"
 )
 
+results = {}
+
 for website in websites:  # website의 단어는 내가 정하는 거
-    if website.startswith("https://"):
-        print("good to go")
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    response = get(website)
+    if response.status_code == 200:
+        print(f"{website} is OK")
+        results[website] = "OK"
     else:
-        print("we have to fix it")
+        print(f"{website} not OK")
+        results[website] = "FAILED"
+
+print(results)
